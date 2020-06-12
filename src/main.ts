bbfd14c7c1c26d98a7fd454b9f9535100b24c41b
Gilbert Zhuo <gzhuo@jadeworld.com>
2020-06-12 20:54:34 +1000

    '1st'

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import Vue from "vue";
+//@ts-ignore
+import VueMaterial from "vue-material";
+import "vue-material/dist/vue-material.min.css";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+Vue.use(VueMaterial);
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
