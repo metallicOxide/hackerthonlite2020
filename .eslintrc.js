92ef6a6ae154687ede1d1fee31beac80b72ae9ad
Jerry <jerrylu1987@hotmail.com>
2020-06-12 22:55:05 +1000

    fix (ESLintrc): fix settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,6 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    '@typescript-eslint/camelcase': 'off',
-    "no-var": false
+    '@typescript-eslint/camelcase': 'off'
   }
 };
